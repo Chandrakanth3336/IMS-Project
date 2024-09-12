187cf05eb04f52fd476a0a1c732d6d3d9543d971
Chandrakanth3336 <chandrakanthreddie@gmail.com>
2024-09-12 09:50:09 +0530

    Login Service Implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
+import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +12,26 @@ export class LoginComponent {
   
   public loginForm: FormGroup;
 
-  constructor() {
+  constructor(private authService:AuthService) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required,Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')])
+      password: new FormControl('', [Validators.required,Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$/)]),
+  
     });
   }
  
+  login(){
+    console.warn(this.loginForm.value);
+    
+    this.authService.getLogin(this.loginForm.value).subscribe(
+      value=>{
+          alert("Login Successfully")
+      },
+      error=>{
+        alert("Login failed")
+      }
+    )
+  }
   
 }
 
